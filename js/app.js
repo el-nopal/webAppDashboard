ff33a3fbfbd5902f4328f5077e7611bf112742e3
mike <xvx1138@protonmail.com>
2019-02-08 20:22:27 +0100

    complete

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
+// --- VARIABLES ---
 const alertBanner = document.getElementById('alert');
 const hide = document.getElementById('close');
 
+const search = document.getElementById('searchUser');
+const message = document.getElementById('messageUser');
+const sendButton = document.getElementById('send');
 
+// hide alert
 hide.addEventListener ('click', () => {
   alertBanner.style.display = 'none';
+});
+
+// message users
+sendButton.addEventListener('click', () => {
+		if(search.value === "" || message.value === "") {
+		  alert("Error: Fill both fields to send message");
+		} else {
+			alert("Message sent");
+		}
 });
