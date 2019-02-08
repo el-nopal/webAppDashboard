c8f814e99687871694801c7fd8a4e8f50cf802cf
mike <xvx1138@protonmail.com>
2019-02-08 14:32:44 +0100

    looking great

--- js/app.js
+++ js/app.js
@@ -5,9 +5,3 @@ const hide = document.getElementById('close');
 hide.addEventListener ('click', () => {
   alertBanner.style.display = 'none';
 });
-// var closeButton = document.getElementById('close');
-//
-// closeButton.addEventListener('click', () => {
-//   const alertBanner = document.getElementById('alert');
-// 	alert.style.display="none"
-// });
