ff7cf54d4cdd4693185ac16d90837249956c1176
mike <xvx1138@protonmail.com>
2019-02-08 13:08:46 +0100

    js add

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const alertBanner = document.getElementById('alert');
+const hide = document.getElementById('close');
+
+
+hide.addEventListener ('click', () => {
+  alertBanner.style.display = 'none';
+});
+// var closeButton = document.getElementById('close');
+//
+// closeButton.addEventListener('click', () => {
+//   const alertBanner = document.getElementById('alert');
+// 	alert.style.display="none"
+// });
